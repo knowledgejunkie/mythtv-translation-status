55c72f9bd102bde14f84bc644f8110c9c5d2fb49
Nick Morrott <knowledgejunkie@gmail.com>
2015-11-29 00:52:12 +0000

    Factor out MythTV branch links into simple JS functions
    Much cleaner than running sed every time a new release comes out

---
+++ templates/mythtv-trans-tester.js
@@ -0,0 +1,23 @@
+function show_summary_available_branches() {
+    document.write("<b>Display stats for:</b> | ");
+    document.write("<a href=\"../master/\">Master</a> | ");
+    document.write("<a href=\"../0-27-fixes/\">0.27-fixes</a> | ");
+    document.write("<a href=\"../0-26-fixes/\">0.26-fixes</a> | ");
+    document.write("<a href=\"../0-25-fixes/\">0.25-fixes</a> | ");
+    document.write("<a href=\"../0-24-fixes/\">0.24-fixes</a> | ");
+    document.write("<a href=\"../0-23-fixes/\">0.23-fixes</a> | ");
+    document.write("<a href=\"../0-22-fixes/\">0.22-fixes</a> | ");
+    document.write("<a href=\"../0-21-fixes/\">0.21-fixes</a> |<br>");
+}
+
+function show_result_available_branches() {
+    document.write("<b>Display stats for:</b> | ");
+    document.write("<a href=\"../../master/\">Master</a> | ");
+    document.write("<a href=\"../../0-27-fixes/\">0.27-fixes</a> | ");
+    document.write("<a href=\"../../0-26-fixes/\">0.26-fixes</a> | ");
+    document.write("<a href=\"../../0-25-fixes/\">0.25-fixes</a> | ");
+    document.write("<a href=\"../../0-24-fixes/\">0.24-fixes</a> | ");
+    document.write("<a href=\"../../0-23-fixes/\">0.23-fixes</a> | ");
+    document.write("<a href=\"../../0-22-fixes/\">0.22-fixes</a> | ");
+    document.write("<a href=\"../../0-21-fixes/\">0.21-fixes</a> |<br>");
+}
