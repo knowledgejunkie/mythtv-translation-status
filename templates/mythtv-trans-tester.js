205e9bb0887fcc224547fcfa2766c864b22bb106
Nick Morrott <knowledgejunkie@gmail.com>
2016-04-02 03:11:14 +0100

    Updates for 0.28/fixes

--- templates/mythtv-trans-tester.js
+++ templates/mythtv-trans-tester.js
@@ -1,6 +1,7 @@
 function show_summary_available_branches() {
     document.write("<b>Display stats for:</b> | ");
     document.write("<a href=\"../master/\">Master</a> | ");
+    document.write("<a href=\"../0-28-fixes/\">0.28-fixes</a> | ");
     document.write("<a href=\"../0-27-fixes/\">0.27-fixes</a> | ");
     document.write("<a href=\"../0-26-fixes/\">0.26-fixes</a> | ");
     document.write("<a href=\"../0-25-fixes/\">0.25-fixes</a> | ");
